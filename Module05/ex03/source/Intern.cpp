f0b2b65363cf4db16f2f4329648b34c905b6df00
Diogo Filipe Braga Baptista <dbraga-b@c2r6s11.42porto.com>
2024-09-22 15:57:41 +0100

    remove hi

--- Module05/ex03/source/Intern.cpp
+++ Module05/ex03/source/Intern.cpp
@@ -33,7 +33,6 @@ AForm *Intern::makeForm(const std::string &formName, const std::string &target)
 	std::string formNames[] = {"presidential pardon", "robotomy request", "shrubbery creation"};
 	AForm* form;
 	form = NULL;
-			std::cout << "hi 2" << std::endl;
 
 	AForm* formTypes[] = {new PresidentialPardonForm(target), new RobotomyRequestForm(target), new ShrubberyCreationForm(target)};
 	for(int i = 0; i < 3; i++)
@@ -41,13 +40,6 @@ AForm *Intern::makeForm(const std::string &formName, const std::string &target)
 		if (formName == formNames[i])
 		{
 			form = formTypes[i];
-			std::cout << "int :" << i << std::endl;
-			std::cout << "form type" << formNames[i] << std::endl;
-			std::cout << form->getName();
-			std::cout << "Memory :" << std::endl;
-			std::cout << formTypes[i] << std::endl;
-			std::cout << form << std::endl;
-
 		}
 	}
 
@@ -56,13 +48,10 @@ AForm *Intern::makeForm(const std::string &formName, const std::string &target)
 		if (form != formTypes[i])
 			delete formTypes[i];
 	}
-	std::cout << "hi " << std::endl;
 	if (form != NULL)
 	{
-		std::cout << target << std::endl;
-		std::cout << "Intern creates ";
-		std::cout << form->getName();
+		std::cout << target << "Intern creates " << form->getName();
 	}
-	std::cout << "hi 3" << std::endl;
+
 	return form;
 }
